b178e719a752228e96d28a6536dfa4e6aae0fcc0
Maxence <maxence.crosse@ynov.com>
2020-04-11 16:49:54 +0200

    modif supprimer_bloc

--- fonction_test/supprimer_bloc.py
+++ fonction_test/supprimer_bloc.py
@@ -5,7 +5,7 @@ def supprimer_bloc(BLOC,num_list_del):
 
 BLOC=[[1,10,1,1],[5,12,2,1],[6,15,2,1],[22,25,1,1]]
 # x y type valeur
-#print(BLOC)
-#num_list_del = 0
-#supprimer_bloc(BLOC,num_list_del)
-#print(BLOC)
+print(BLOC)
+num_list_del = 0
+supprimer_bloc(BLOC,num_list_del)
+print(BLOC)
