c1b4c0d1c8c757ab423e438515054b502832e3b8
ValentinJ <valentin.joassard@ynov.com>
2020-05-02 16:08:18 +0200

    definition des coordonnees

--- test2.py
+++ test2.py
@@ -8,15 +8,15 @@ def clavier(event):
     gauche = -25
     droite = 25
     move = event.keysym
-    if move == "Right":
+    if move == "Right" and coords[0]<700 :
         coords = (coords[0] + 25, coords[1])
         canvas.move(player, droite, 0)
-    elif move == "Left":
+    elif move == "Left" and coords[0]>80:
         coords = (coords[0] -25, coords[1])
         canvas.move(player, gauche, 0)
     elif move == "Up":
         t=PhotoImage(file="image/carre.png")
-        print(coords)
+        #print(coords)
         shot = canvas.create_image(coords[0],coords[1]-25,image=t)
         xTir = coords[0]
         yTir = coords[1]
@@ -25,6 +25,7 @@ def clavier(event):
         print(Liste)
     elif move =="Down":
         canvas.coords(shot,coords[0],coords[1]-25)
+
     #while(yTir!=0):
     #    canvas.coords(shot,coords[0],coords[1]-25)
     #    yTir = coords[1]-25            
@@ -36,30 +37,17 @@ def clavier(event):
 # création du canvas
 canvas = Canvas(fenetre, width=768, height=576, bg="ivory")
 # coordonnées initiales
-coords = (490,520)
+coords = (390,520)
 # création du rectangle
 bg=PhotoImage(file='image/espace1.png')
 background= canvas.create_image(370,250,image=bg)
 p=PhotoImage(file='image/rocket.png')
-player =  canvas.create_image(490,520,image=p)
+player =  canvas.create_image(390,520,image=p)
 # ajout du bond sur les touches du clavier
 canvas.focus_set()
 canvas.bind("<Key>", clavier)
 # création du canvas
 canvas.pack()
-
-
-
-
-
-
-
-
-
-
-
-
-
 champ_label = Label(fenetre, text="Brick shooter")
 champ_label.pack()
 bouton_quitter=Button(fenetre, text="Quitter", command=fenetre.quit)
