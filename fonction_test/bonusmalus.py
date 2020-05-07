74105d77bc179744cb62e2fb1c9d9f42cfd366cc
Maxence <maxence.crosse@ynov.com>
2020-05-07 10:13:59 +0200

    Début enregistrement

--- fonction_test/bonusmalus.py
+++ fonction_test/bonusmalus.py
@@ -1,19 +1,20 @@
 import os
 import pickle
 os.chdir("c:/Users/Maxence CROSSE/Desktop/ProjetLogiciel/Projet-logiciel/Projet-logiciel/fonction_test")
+score = {
+  "joueur 1":    5,
+  "joueur 2":   35,
+  "joueur 3":   20,
+  "joueur 4":    2,
+    }
+with open('fichier.txt', 'wb') as fichier:
+    mon_pickler = pickle.Pickler(fichier)
+    mon_pickler.dump(score)
+
 with open('fichier.txt', 'rb') as fichier:
     mon_depickler = pickle.Unpickler(fichier)
     score_recupere = mon_depickler.load()
-    
-x= 10
-y = 15
-def enregistrement(x,y):
-    stats = {
-        x,
-        y,
-    }
-    with open('fichier.txt', 'wb') as fichier:
-        mon_pickler = pickle.Pickler(fichier)
-        mon_pickler.dump(stats)
+    print(score_recupere)
 
-enregistrement(x,y)
+
+
