513761cf323db0798146f64d8bb8f1ea76c26946
Maxence <maxence.crosse@ynov.com>
2020-05-08 12:21:10 +0200

    Début lien de pages

--- fonction_test/menuPrincipal.py
+++ fonction_test/menuPrincipal.py
@@ -1,72 +1,541 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import Tk ,Canvas
+import tkinter.font as tkFont
+from winsound import *
+from random import randint
+#---------------------------#
+      #Variable Globale#
+#---------------------------#
+  
 
-menu = Tk()
-menu_fenetre = Canvas(menu, width=768, height=576, bg="white")
-
-
-
-fenetre = Tk()
-canvas = Canvas(fenetre, width=768, height=576, bg="ivory")
-# coordonnées initiales
-#coords = (0, 0)
-# création du rectangle
-bg=PhotoImage(file='../image/espace1.png')
-background= canvas.create_image(370,250,image=bg)
-p=PhotoImage(file='../image/rocket.png')
-player =  canvas.create_image(490,520,image=p)
-# ajout du bond sur les touches du clavier
-canvas.focus_set()
-canvas.bind("<Key>", clavier)
-# création du canvas
-
-
-champ_label = Label(fenetre, text="Brick shooter")
-champ_label.pack()
-bouton_quitter=Button(fenetre, text="Quitter", command=fenetre.quit)
-bouton_quitter.pack(side="bottom")
-bouton_accueil=Button(fenetre,text="Accueil")
-bouton_accueil.pack(side="bottom")
-bouton_accueil.configure( width=15, height=3,  )
-bouton_quitter.configure( width=15, height=3,  )
-cadre = Frame(fenetre)
-cadre.pack(side="bottom", fill=BOTH)
-fenetre.attributes('-fullscreen', True)
+bx=0
+by=5
+dx = 0
+dy = -5
+x=5
+Var1=0
+Var2=0
+var3=0
+fin=3
+score=0
+vitessechute=1000
+vitesseapparition=2000
+gauche = -25
+droite = 25
+piece=0
+gain=0
+meilleur_score = 0
+anticheat=0
+anticheat1=0
+anticheat2=0
+ok=0
+coords=(390,520)
 
 
-def clavier(event):
-    global coords
+def quitter():
+    menu.destroy()
+
+def creer_fen(fen, text_label):
+    label = tk.Label(fen, text=text_label)
+    button = tk.Button(fen, text="Revenir vers root", command=changer_fen(False, True, fen))
+    label.grid(column=0, row=0)
+    button.grid(column=0, row=1)
+    fen.protocol("WM_DELETE_WINDOW", quitter)
+
+
+
+def lancer_jeu():
+    #---------------------------#
+        #Variable Globale#
+    #---------------------------#
+    
+
+    bx=0
+    by=5
+    dx = 0
+    dy = -5
+    x=5
+    Var1=0
+    Var2=0
+    var3=0
+    fin=3
+    score=0
+    vitessechute=1000
+    vitesseapparition=2000
     gauche = -25
     droite = 25
+    piece=0
+    gain=0
+    meilleur_score = 0
+    anticheat=0
+    anticheat1=0
+    anticheat2=0
+    ok=0
+    #-------------------------------#
+        #Enregistrement/chargement#
+    #-------------------------------#
 
-    move = event.keysym
-    if move == "Right":
-        #coords = (coords[0] + 2, coords[1])
-        canvas.move(player, droite, 0)
-    elif move == "Left":
-        #coords = (coords[0] -2, coords[1])
-        canvas.move(player, gauche, 0)
+    def enregistrer():
+        global meilleur_score, piece
+        var = str(meilleur_score)
+        var1 = str(piece)
+        mon_fichier = open("fichier.txt", "w")
+        mon_fichier.write(var)
+        mon_fichier.write("\n")
+        mon_fichier.write(var1)
+        mon_fichier.close()
+
+    def charger():
+        global meilleur_score, piece
+        with open("fichier.txt", "r") as fichier:
+            meilleur_score, piece = [int(elt) for elt in fichier.readlines()]
+
+    #-------------------------#
+        # Affichage #
+    #-------------------------#
+
+    def afficherpiece(piece):
+        Coin=Label(fenetre,image=coin,bg="#404040")
+        Coin.place(x=1250, y =100)
+        fontStyle = tkFont.Font(family="Eras Demi ITC", size=20)
+        Credit.config(text='Pieces : ' + str(piece), font=fontStyle)
+        Credit.place(x=1300, y =100)
+
+    def gagnerpiece():
+        global piece,gain
+        gain+=1
+        if gain==10:
+            piece+=1
+            gain=0
+        afficherpiece(piece)
+
+
+    def affichagescore():
+        Explosion=Label(fenetre,image=explosion,bg="#404040")
+        Explosion.place(x=120, y =200)
+        fontStyle = tkFont.Font(family="Eras Demi ITC", size=20)
+        Resultat.config(text='  Score : ' + str(score), font=fontStyle)
+        Resultat.place(x=150, y =200)
+
+
+    def affichage_meilleur_score(): 
+        global meilleur_score
+        Coupe=Label(fenetre,image=coupe,bg="#404040")
+        Coupe.place(x=1200, y =195)
+        fontStyle = tkFont.Font(family="Eras Demi ITC", size=20)
+        
+        MeilleurScore.config(text='Meilleur score : ' + str(meilleur_score), font=fontStyle)
+        MeilleurScore.place(x=1250, y =200)
+
+
     
-    fenetre.update()
+
+    def affichervie(Nvie1,Nvie2,Nvie3):
+        global fin,ok
+        
+        
+        if ok==0:
+        
+            Nvie1.place(x=165, y =100)
+            Nvie2.place(x=225, y =100)
+            Nvie3.place(x=285, y =100)
+            ok=ok+1
+        if fin==2 and ok==1:
+            Nvie3.place_forget()
+            ok=ok+1
+        if fin==1 and ok==2:
+            Nvie2.place_forget()
+            ok=ok+1
+        if fin==0 and ok==3:
+            Nvie1.place_forget()
+            ok=ok+1
+        fontStyle = tkFont.Font(family="Eras Demi ITC", size=20)
+        Vie.config(text=' Vie : ', font=fontStyle)
+        Vie.place(x=100, y =100)
 
 
-def play():
+    #---------------------------------------------#
+        #Création et déplacement des bricks#
+    #---------------------------------------------#
+
+    #Accélère la vitesse de chute des blocs en fonctions du temps
+    def vitesse_chute():
+        global vitessechute
+        if vitessechute>850:
+            vitessechute=vitessechute-70
+        if vitessechute>750 and vitessechute<851:
+            vitessechute=vitessechute-50
+        if vitessechute>500 and vitessechute<751:
+            vitessechute=vitessechute-20
+        if vitessechute>80 and vitessechute<501:
+            vitessechute=vitessechute-1
+
+    #Accélère la vitesse d'apparition des blocs en fonctions du temps
+    def vitesse_apparition():
+        global  vitesseapparition
+        if vitesseapparition>1500:
+            vitesseapparition=vitesseapparition-50
+        if vitesseapparition>1000 and vitesseapparition<1501:
+            vitesseapparition=vitesseapparition-10
+        if vitesseapparition>800 and vitesseapparition<1001:
+            vitesseapparition=vitesseapparition-5
+        if vitesseapparition>750 and vitesseapparition<801:
+            vitesseapparition=vitesseapparition-1
+
+    #Défini un type de bloc et l'affiche
+    def create_brick():
+        global brick,x1,x2,var3
+        proba = randint(0,100)
+        x1 = randint(80,700)
+        x2 = x1 + 30
+        if (proba >=0 and proba<=70):
+            type = 0
+            brick = canvas.create_rectangle(x1,100,x2,130,fill="red")
+        if (proba >70 and proba<=80):
+            type = 1
+            brick = canvas.create_rectangle(x1,100,x2,130,fill="blue")
+        if (proba >80 and proba<=90):
+            type = 2
+            brick = canvas.create_rectangle(x1,100,x2,130,fill="green")
+        if (proba >90 and proba<=100):
+            type = 3
+            brick = canvas.create_rectangle(x1,100,x2,130,fill="yellow")
+        
+        BRICK.append([brick,type])
+        vitesse_apparition()
+        fenetre.after(vitesseapparition,create_brick)
+
+    #Déplacement du tir vers le haut
+    def deplacement():
+        global dx, dy, Var1, Var2,x
+        if Var1==0:
+            canvas.move(shot,dx,dy)
+            destroy_bloc(shot)
+            fenetre.after(x,deplacement)
+            Var1 = 1
+            Var2 = 0
+            if canvas.coords(shot) != []:
+                Var1 = 0
+                Var2 = 1
+                if canvas.coords(shot)[1]<50:
+                    canvas.delete(shot)
+                    Var1=1
+                    Var2=0
+
+    #Déplacement des blocs vers le bas
+    def descente():
+        global bx,by,fin
+        if fin!=0:
+            nb = -1
+
+            while(nb<len(BRICK)-1):
+                if (canvas.coords(BRICK[nb][0])[1]==500):
+                    canvas.delete(BRICK[nb][0])
+                    del BRICK[nb]
+                    nb+=1
+                    if fin>0:
+                        fin-=1
+                    affichervie(Nvie1,Nvie2,Nvie3)
+                    lose(fin)
+                else:
+                    canvas.move(BRICK[nb][0],bx,by)
+                    nb +=1
+            vitesse_chute()
+            fenetre.after(vitessechute,descente)
+
+    #--------------------------------#
+        # Destruction des bricks #
+    #--------------------------------#
+
+    #Destruction des blocs s'ils touchent un tir
+
+    def destroy_bloc(shot):
+        global Var1,Var2,score,x,by
+        midShot = (canvas.coords(shot)[0] + canvas.coords(shot)[2])/2
+        nb = -1
+        bug=0     
+        if(len(BRICK)!=0):
+            while(nb<len(BRICK)-1) and bug==0:
+                bug=1
+                if canvas.coords(shot)!=[] :
+                    bug=0 
+                    if( canvas.coords(shot)[1]<=canvas.coords(BRICK[nb][0])[3] and canvas.coords(shot)[1]>=canvas.coords(BRICK[nb][0])[1] and canvas.coords(BRICK[nb][0])[0]-20<=midShot and midShot<=canvas.coords(BRICK[nb][0])[2]+20):
+                        canvas.delete(shot)
+                        canvas.delete(BRICK[nb][0])
+                        if(BRICK[nb][1]==1):
+                            nerf_vit_tir()
+                        if(BRICK[nb][1]==2):
+                            up_vit_dep()
+                        if(BRICK[nb][1]==3):
+                            up_vit_defil()
+                        del BRICK[nb]
+                        nb +=1
+                        Var1= 0
+                        Var2=0
+                        score=score+1
+                        affichagescore()
+                        gagnerpiece()
+                    else:
+                        nb+=1
+                        Var1 =0
+                        Var2=0
+
+    #---------------------------#
+        # Bonus - Malus #
+    #---------------------------#
+
+    def up_vit_tir():
+        global x
+        x = 2
+        fenetre.after(10000,restat_up_vit_tir)
+
+    def restat_up_vit_tir():
+        global x
+        x=5
+
+    def up_vit_defil():
+        global vitessechute,anticheat1
+        Up_chute1.place(x=200,y=303)
+        if anticheat1==1:
+            Up_chute2.place(x=250,y=303)
+
+        if anticheat1<2:
+            anticheat1+=1
+            vitessechute-=20        
+            fenetre.after(10000,restat_up_vit_defil)
+
+    def restat_up_vit_defil():
+        global anticheat1,vitessechute
+        anticheat1-=1
+        vitessechute+=20
+        if anticheat1==1:
+            Up_chute2.place_forget()
+        if anticheat1==0:
+            Up_chute1.place_forget()
+        
+    def nerf_vit_tir():
+        global x,anticheat2
+        
+        Down.place(x=200,y=463)
+        if anticheat2==1:
+            Down3.place(x=250,y=463)
+
+        if anticheat2<2:
+            anticheat2+=1
+            x+=5
+            fenetre.after(10000,restat_nerf_vit_tir)
+
+    def restat_nerf_vit_tir():
+        global x,anticheat2
+        anticheat2-=1
+        x-=5
+        if anticheat2==1:
+            Down3.place_forget()
+        if anticheat2==0:
+            Down.place_forget()
+
+    def up_vit_dep():
+        global gauche, droite,anticheat
+        Up_vit1.place(x=200,y=383)
+        if anticheat==1:
+            Up_vit2.place(x=250,y=383)
+        if anticheat<2:        
+            anticheat+=1
+            gauche -=15
+            droite +=15
+            fenetre.after(10000,restat_up_vit_dep)
+
+    def restat_up_vit_dep():
+        global gauche, droite,anticheat
+        anticheat-=1
+        gauche+=15
+        droite-=15
+        if anticheat==1:
+            Up_vit2.place_forget()
+        if anticheat==0:
+            Up_vit1.place_forget()
+
+    #---------------------------#
+            # Défaite #
+    #---------------------------#
+
+    def lose(fin):
+        global meilleur_score, score
+        if fin == 0:
+            fontStyle = tkFont.Font(family="Arial Black", size=50)
+            Fin = Label(fenetre, fg ='red',bg='#074e6c')
+            Fin.config(text='      GAME OVER      \nTon score: ' + str(score)+" ", font=fontStyle)
+            Fin.place(x=410, y =150)
+            if meilleur_score < score :
+                Champion = Label(fenetre, fg ='red',bg='#074e6c')
+                Champion.config(text='     NEW RECORD     ', font=fontStyle)
+                Champion.place(x=405, y =400)
+                meilleur_score = score
+            enregistrer()
+
+    #-------------------------------#
+        # Déplacement du joueur #
+    #-------------------------------#
+
+    #Fonction pour aller à gauche, droite ou tirer
+    def clavier(event):
+        global coords, shot, Var1, Var2, gauche, droite
+        move = event.keysym
+        if move == "Right" and coords[0]<700 :
+            coords = (coords[0] + droite, coords[1])
+            canvas.move(player, droite, 0)
+        elif move == "Left" and coords[0]>80:
+            coords = (coords[0] + gauche, coords[1])
+            canvas.move(player, gauche, 0)
+        elif move == "Up":
+            Var1=0
+            if Var2==0:
+                shot = canvas.create_oval(coords[0]-15,coords[1]+45,coords[0]+15,coords[1]+10,fill='red')
+                Var2=1
+                deplacement()
+            fenetre.update()
+        
+    #---------------------------#
+        #Code principal#
+    #---------------------------#
+
+    fenetre = Tk() 
+    charger()
+    BRICK= []
+
+    #---------------------------#
+            #Images#
+    #---------------------------#
+
+    coin=PhotoImage(master=fenetre,file='image/coin.png')
+    coupe=PhotoImage(master=fenetre,file='image/coupe.png')
+    explosion=PhotoImage(master=fenetre,file='image/epee.png')
+    p=PhotoImage(master=fenetre,file='image/rocket1.png')
+    f=PhotoImage(master=fenetre,file='image/coeur.png')
+    image=PhotoImage(master=fenetre,file='image/espace5.png')
+    titre=PhotoImage(master=fenetre,file='image/titre.png')
+    images=PhotoImage(master=fenetre,file='image/espace5.png')
+    down=PhotoImage(master=fenetre,file='image/down.png')
+    up=PhotoImage(master=fenetre,file='image/up.png')
+    bg=PhotoImage(master=fenetre,file='image/espace3.png')
+
+    #---------------------------#
+        #Affichage-label#
+    #---------------------------#
+
+    fonds=Label(fenetre,image=images)
+    fonds.place(x=-205, y =-300)
+    fontStyle = tkFont.Font(family="Eras Demi ITC", size=20)
+    MeilleurScore = Label(fenetre, fg ='#FFD700',bg="#404040")
+    Vie = Label(fenetre, fg ='#0c136d',bg='#404040')
+    Resultat = Label(fenetre, fg ='#0c136d',bg='#404040')
+    Credit=Label(fenetre, fg= "#FFD700",bg='#404040')
+    Titre=Label(fenetre,image=titre,bg="black")
+    Nvie1=Label(fenetre,image=f,bg="#404040")
+    Nvie2=Label(fenetre,image=f,bg="#404040")
+    Nvie3=Label(fenetre,image=f,bg="#404040")
+    Up_chute1=Label(fenetre,image=up,bg="black")
+    Up_chute2=Label(fenetre,image=up,bg="black")
+    Up_vit1=Label(fenetre,image=up,bg="black")
+    Up_vit2=Label(fenetre,image=up,bg="black")
+    Down=Label(fenetre,image=down,bg="black")
+    Down3=Label(fenetre,image=down,bg="black")
+    Down2=Label(fenetre,fg='#0c136d',bg="#404040")
+    Down2.config(text="       tir       ", font=fontStyle)
+    Down2.place(x=50,y=460)
+    Up2=Label(fenetre,fg='#0c136d',bg="#404040")
+    Up2.config(text="   vitesse   ", font=fontStyle)
+    Up2.place(x=50,y=380)
+    Up3=Label(fenetre,fg='#0c136d',bg="#404040")
+    Up3.config(text="    chute    ", font=fontStyle)
+    Up3.place(x=50,y=300)
+    Titre.place(x=400, y =620)
+
+    affichervie(Nvie1,Nvie2,Nvie3)
+    affichagescore()
+    afficherpiece(piece)
+    affichage_meilleur_score()
+
+    #--------------------------------#
+        #Création du cadre jouable#
+    #--------------------------------#
+
+    canvas =Canvas(fenetre, width=768, height=576, bg="ivory")
+    #coords = (390,520)
+    canvas.focus_set()
+    canvas.bind("<Key>", clavier)
     canvas.pack()
-    fenetre.pack()
+
+    #---------------------------#
+        #Fond d'écran - Skin#
+    #---------------------------#
+
+    background= canvas.create_image(370,250,image=bg)
+    player =  canvas.create_image(390,520,image=p)
+
+    #---------------------------#
+        #Lancement du jeu#
+    #---------------------------#
+
+    create_brick()
+    descente()
+
+    #---------------------------#
+            #Boutons#
+    #---------------------------#
+
+    bouton_quitter=Button(fenetre, text="Quitter", command=fenetre.quit)
+    bouton_quitter.pack(side="bottom")
+    bouton_accueil=Button(fenetre,text="Accueil")
+    bouton_accueil.pack(side="bottom")
+    bouton_accueil.configure( width=15, height=3,  )
+    bouton_quitter.configure( width=15, height=3,  )
+    cadre = Frame(fenetre)
+    cadre.pack(side="bottom", fill=BOTH)
+
+    #---------------------------#
+
+    fenetre.attributes('-fullscreen', True)
+    fenetre.mainloop()
+    enregistrer()
+
+
+def changer_fen(in_menu, in_a_fen, fen):
+    def changer_utilite():
+        if in_menu and not in_a_fen:
+            menu.withdraw()
+            fen.deiconify()
+        else:
+            fen.withdraw()
+            menu.deiconify()
+    return changer_utilite
 
 
 
 
 
 
-bouton_play=Button(menu, text="Jouer", command=play)
-bouton_play.pack()
- 
-bouton_tuto=Button(menu, text="Tutoriel")#, command=tuto)
-bouton_tuto.pack()
- 
-bouton_quit=Button(menu, text="Quitter")#, command=menu.destroy)
-bouton_tuto.pack()
+menu = tk.Tk()
 
-menu_fenetre.pack()
-menu_fenetre.mainloop()
+fen_jouer = tk.Toplevel(menu)
+fen_tuto = tk.Toplevel(menu)
+fen_skin = tk.Toplevel(menu)
+
+creer_fen(fen_jouer, "Je suis dans la fenetre a")
+creer_fen(fen_tuto, "Je suis dans la fenetre b")
+creer_fen(fen_skin, "Je suis dans la fenetre c")
+
+fen_jouer.withdraw()
+fen_tuto.withdraw()
+fen_skin.withdraw()
+
+fen_jouer_button = tk.Button(menu, command=lancer_jeu, text="Play")
+fen_tuto_button = tk.Button(menu, command=changer_fen(True, False, fen_tuto), text="Tuto")
+fen_skin_button = tk.Button(menu, command=changer_fen(True, False, fen_skin), text="Skin")
+
+fen_jouer_button.grid()
+fen_tuto_button.grid(column=0, row=1)
+fen_skin_button.grid(column=0, row=2)
+  
+menu.mainloop()
---
+++ fonction_test/navigationFen.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+  
+def clean_exit():
+    root.destroy()
+  
+def alternate_window(is_in_root, is_in_window, window):
+    def alternate_processing():
+        if is_in_root and not is_in_window:
+            root.withdraw()
+            window.deiconify()
+        else:
+            window.withdraw()
+            root.deiconify()
+    return alternate_processing
+  
+def create_window(window, text_label):
+    label = tk.Label(window, text=text_label)
+    button = tk.Button(window, text="Revenir vers root", command=alternate_window(False, True, window))
+    label.grid(column=0, row=0)
+    button.grid(column=0, row=1)
+    window.protocol("WM_DELETE_WINDOW", clean_exit)
+  
+root = tk.Tk()
+  
+a_window = tk.Toplevel(root)
+b_window = tk.Toplevel(root)
+c_window = tk.Toplevel(root)
+  
+create_window(a_window, "Je suis dans la fenetre a")
+create_window(b_window, "Je suis dans la fenetre b")
+create_window(c_window, "Je suis dans la fenetre c")
+  
+a_window.withdraw()
+b_window.withdraw()
+c_window.withdraw()
+  
+a_button = tk.Button(root, command=alternate_window(True, False, a_window), text="affiche fenetre a")
+b_button = tk.Button(root, command=alternate_window(True, False, b_window), text="affiche fenetre b")
+c_button = tk.Button(root, command=alternate_window(True, False, c_window), text="affiche fenetre c")
+  
+a_button.grid()
+b_button.grid(column=1, row=0)
+c_button.grid(column=2, row=0)
+  
+root.mainloop()
